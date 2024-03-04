0ebed2331075ee579941bc5992f9aacc5816f40a
YumNumm <onory0917@gmail.com>
2024-03-04 18:51:38 +0900

    add: WidgetKit

---
+++ app/ios/Widget/WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by 尾上 遼太朗 on 2024/03/04.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+        WidgetLiveActivity()
+    }
+}
